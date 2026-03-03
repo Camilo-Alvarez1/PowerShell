c83496ad581cab051ef3bb5dab8761b5d7ac86e6
Camilo-Alvarez1 <alvarez.camilo55@gmail.com>
2026-03-03 14:04:04 -0800

    Initial Commit

---
+++ MyChallenges/mystuff.ps1
@@ -0,0 +1,5 @@
+﻿# Task 1
+Get-ADUser -Filter * -Properties * |  Select-Object Name, @{n="NameLength"; e={$_.name.length}} | sort NameLength -Descending | select -Last 10
+
+# Task 2
+Get-ADUser -Filter * -Properties * | select lastlogon @{n="lastlogon"; e={
