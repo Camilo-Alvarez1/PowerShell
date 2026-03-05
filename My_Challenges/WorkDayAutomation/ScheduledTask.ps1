03e317dac7e558e424cce4af3cace827bd8b36ec
Camilo-Alvarez1 <Alvarez.camilo55@gmail.com>
2026-03-04 16:10:53 -0800

    Commit

--- My_Challenges/WorkDayAutomation/ScheduledTask.ps1
+++ My_Challenges/WorkDayAutomation/ScheduledTask.ps1
@@ -3,7 +3,7 @@ $action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-NoProfil
 # Sets trigger to 7:50 AM
 $trigger = New-ScheduledTaskTrigger -Daily -At 7:50AM
 # Changes the trigger from every day (7 Days a week) to Mon-Thur using an aray of days @()
-$trigger.DaysOfWeek = @("Monday","Tuesday","Wednesday","Thursday")
+$trigger.daysofweek = @("Monday","Tuesday","Wednesday","Thursday")
 
 Register-ScheduledTask -TaskName "Start Workday Apps" -Action $action -Trigger $trigger -Description "Opens all work applications at 7:50 AM on weekdays exept Friday"
 
