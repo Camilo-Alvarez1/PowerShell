7feeacb317e5b49386a331ea9046aa98094216ea
Camilo-Alvarez1 <alvarez.camilo55@gmail.com>
2026-03-16 14:27:41 -0700

    Commit

--- ProjectWeek/VMs/CreateVMs.ps1
+++ ProjectWeek/VMs/CreateVMs.ps1
@@ -1,9 +1,9 @@
 # Configuration
 $VMName = "Server1"
 $VMRoot = "C:\HyperV\VMs"
-$VMPath = "$VMName\$VMName"
+$VMPath = "$VMRoot\$VMName"
 $VHDPath = "$VMPath\$VMName.vhdx"
-$ISOPath = "C:\ISOs\WindowsServer2022.iso" # Update ISO file location
+$ISOPath = "C:\ISO\SERVER_EVAL_x64FRE_en-us.iso" # Update ISO file location
 
 $MemoryStartup = 4GB
 $VHDSize = 15GB
@@ -15,12 +15,25 @@ if (-not (Test-Path $VMPath)) {
 }
 
 # Import Hyper-V Module
-Import-Module Hyper-V
-Get-Command -Module Hyper-V
-Get-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-All
-Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-All -All
-
-
+# Import-Module Hyper-V # Needs PowerShell 5 not 7
 
 # Create the VM
-New-VM -n
+New-VM -Name $VMName -MemoryStartupBytes $MemoryStartup -Generation 2 -Path $VMPath
+
+# Create and attach a Virtual Hard Disk
+New-VHD -Path $VHDPath -SizeBytes $VHDSize -Dynamic
+Add-VMHardDiskDrive -VMName $VMName -Path $VHDPath
+
+# Attach the Windows Server ISO
+Add-VMDvdDrive -VMName $VMName -Path $ISOPath
+
+# Set the CPU count
+Set-VMProcessor -VMName $VMName -Count $CPUCount
+
+# Connect the VM to the defult switch
+Connect-VMNetworkAdapter -VMName $VMName -SwitchName "Default Switch"
+
+# Start the VM
+Start-VM -Name $VMName
+
+
